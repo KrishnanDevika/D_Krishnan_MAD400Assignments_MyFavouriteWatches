a2ebd69f3518c9fd7a7780bded2774cf4838b5f0
Devika Krishnan <w0774427@myscc.ca>
2022-02-13 11:47:43 -0500

    Changes in Assignment2

--- D_Krishnan_MyFavouriteWatches/src/app/content-card/content-card.component.ts
+++ D_Krishnan_MyFavouriteWatches/src/app/content-card/content-card.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Content } from '../helper-files/content-interface';
 
 
@@ -9,43 +9,18 @@ import { Content } from '../helper-files/content-interface';
   styleUrls: ['./content-card.component.scss']
 })
 export class ContentCardComponent implements OnInit {
+  @Input() watch?: Content;
 
 
 
   constructor() { 
-    let fossil : Content;
-    fossil = {
-      id : 1,
-      title : "Fossil",
-      description : "A nice Rose Gold Stainless Steel watch with round dial",
-      creator : "Fossil Smart Watch",
-      imgURL : "https://www.fossil.com/en-in/products/carlie-three-hand-rose-gold-tone-stainless-steel-watch/ES4301.html",
-      type : "Smart Watch"
-    }
-
-    let rolex : Content;
-    rolex = {
-      id : 2,
-      title : "Rolex",
-      description : "A square shaped dial with black leather strap watch nice to wear",
-      creator : "Rolex Analog",
-      imgURL : "https://www.1stdibs.com/jewelry/watches/wrist-watches/rare-unusual-rolex-vintage-precision-stainless-steel-3327042/",
-      type : "Analog Watch"
-    }
-
-    let titan : Content;
-    titan = {
-      id : 3,
-      title : "Titan",
-      description : "A Stainless Steel oval shaped dial with clock digital classy look",
-      creator : "Titan Digital",
-      imgURL : "https://www.myntra.com/watches/titan/titan-raga-women-white-dial-watch-ne2370ym03/240199/buy",
-      type : "Digital Watch"
-    }
-
   }
 
   ngOnInit(): void {
+  }
+
+  ImageClick(index : number){
+    console.log(this.watch?.id, this.watch?.title)
   }
 
 }
